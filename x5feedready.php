fc73ddb545e12dd46c27a2af9234a8fc2d5f6bee
austinpower1 <108170047+austinpower1@users.noreply.github.com>
2022-06-26 13:34:34 +0200

    Add files via upload

---
+++ x5feedready.php
@@ -0,0 +1,31 @@
+<?php include('res/x5engine.php'); header('Content-Type: text/xml'); echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
+<rss version="2.0">
+	<channel>
+		<title><![CDATA[IMAGLOBECREATIVE]]></title>
+		<link>http://imaglobecreative.com</link>
+		<description><![CDATA[Professional  photographic and video production In Berlin Germany with low cost, we will arrange your project accounding to what you can offer and still get professional satifactions... with longtime free update...
+
+and It's all about creativity..... Your creative ability determine your mind ability....]]></description>
+		<image>
+			<url>http://imaglobecreative.com/images/pdf-copy.png</url>
+			<title><![CDATA[IMAGLOBECREATIVE]]></title>
+			<link>http://imaglobecreative.com</link>
+		</image>
+		<language>EN</language>
+		<lastBuildDate><?php echo getLastAvailableDate(array(1656231600)); ?></lastBuildDate>
+		<generator>Incomedia WebSite X5 Professional</generator>
+		<category>imfeed</category>
+<?php if (time() > 1656231600): ?>
+		<item>
+			<title><![CDATA[GrassRoot Communities]]></title>
+			<author><![CDATA[AustinPower]]></author>
+			<category domain="http://imaglobecreative.com/blog/index.php?category=News"><![CDATA[News]]></category>
+			<category>imblog</category>
+			<description><![CDATA[<div id="imBlogPost_000000004"></div>]]></description>
+			<pubDate>Sun, 26 Jun 2022 08:20:00 GMT</pubDate>
+			<link>http://imaglobecreative.com/blog/?grassroot-communities</link>
+			<guid isPermaLink="false">http://imaglobecreative.com/blog/rss/000000004</guid>
+		</item>
+<?php endif; ?>
+	</channel>
+</rss>
